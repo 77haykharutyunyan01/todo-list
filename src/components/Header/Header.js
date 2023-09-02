4dde3ae30149f28f69a09cb11161ab12d6cddd1e
Hayk Harutyunyan <hayk_harutyunyan_2001@mail.ru>
2023-09-02 17:52:24 +0400

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
+import { Component } from 'react';
 import "./header.css"
-const Header = () =>{
-    return (
-    <div className="header">
-        <h1> My Todo List <span>Done:12  Important:34</span></h1>
-        </div>
-    )
+class Header extends Component{
+    render(){
+        return (
+            <div className="header">
+                <h1> My Todo List <span>Done:12  Important:34</span></h1>
+                </div>
+            )
+    }
 }
 export default Header
