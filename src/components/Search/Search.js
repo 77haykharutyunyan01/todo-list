c103b4c75cf5804e8965e73b384a9e3ba3a2a9b3
Hayk Harutyunyan <hayk_harutyunyan_2001@mail.ru>
2023-09-02 23:48:11 +0400

    first commit

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,15 @@
-
+import { Component } from "react"
 import "./search.css"
-const Search = () => {
-    return (
-    <div className="search">
-        <input type="text" placeholder="search"/>
-        <button className="search-btn-all">All</button>
-        <button className="search-btn-done">Done</button>
-        <button className="search-btn-important">Important</button>
-    </div>
-    )
+export default class Search extends Component {
+    render() {
+        return (
+            <div className="search">
+                <input type="text" placeholder="search" />
+                <button className="search-btn-all">All</button>
+                <button className="search-btn-done">Done</button>
+                <button className="search-btn-important">Important</button>
+            </div>
+        )
+    }
 }
-export default Search
+
